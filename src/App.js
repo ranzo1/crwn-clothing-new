e2a1445295bb91b2dc998a70f642be360504783c
Zoran Protic <zoran.protic@payspot.co.rs>
2021-12-08 08:20:46 +0100

    implemented Router

--- src/App.js
+++ src/App.js
@@ -2,11 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 import HomePage from '../src/pages/homepage/homepage.component';
+import { Switch, Route } from 'react-router-dom';
+
+const HatsPage = (props) => (
+  <div>
+    {console.log(props)}
+    <h1>HATS PAGE </h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import MenuItem from '../menu-item/menu-item.component';
 import './directory.styles.scss';
+import { withRouter } from 'react-router-dom';
 
 const sections = [
     {
@@ -50,8 +51,8 @@ class Directory extends React.Component {
 render() {
     return (
         <div className='directory-menu'>
-            {this.state.sections.map(({title, imageUrl, id, size}) => (
-                <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+            {this.state.sections.map(({id, ...otherSectionProps}) => (
+                <MenuItem key={id} {...otherSectionProps}/>
             ))}
         </div>
     ); 
