797bf7df05ed6d026d1e56732fe22cc667572d92
Zoran Protic <zoran.protic@payspot.co.rs>
2021-12-08 09:56:59 +0100

    added shop page with collection items

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import './App.css';
 
 import HomePage from '../src/pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
-
-const HatsPage = (props) => (
-  <div>
-    {console.log(props)}
-    <h1>HATS PAGE </h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/shop/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
